957b5b48ba5b1977f2a7d14363c7a1198736276f
Charltonortega <charltonortega14@gmail.com>
2023-07-12 22:42:34 +0930

    feat: comment updates

--- assets/js/end.js
+++ assets/js/end.js
@@ -1,40 +1,50 @@
 // Selecting DOM elements
-var username = document.querySelector('#username');
+var usernameInput = document.querySelector('#username');
 var saveScoreBtn = document.querySelector('#saveScoreBtn');
 var finalScore = document.querySelector('#finalScore');
 var mostRecentScore = localStorage.getItem('mostRecentScore');
 
+//get high scors from local storage, PARSE it into an array, then assigned into highScores
 var highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 
-var MAX_HIGH_SCORES = 6;
+// max amount of highscores to be displayed
+var maxHighScores = 6;
 
+// Display the most in large letters recent score
 finalScore.innerText = mostRecentScore;
 
 /* Disabling the saveScoreBtn until the user types something in the username input. */
-username.addEventListener('keyup', () => {
-    saveScoreBtn.disabled = !username.value;
+usernameInput.addEventListener('keyup', function() {
+    saveScoreBtn.disabled = !usernameInput.value;
 });
 
 /* Preventing the default action of the event. */
-saveHighScore = e => {
-    e.preventDefault();
+function saveHighScore(event) {
+    event.preventDefault();
 
     /* Creating an object with the score and name. */
     var score = {
         score: mostRecentScore,
-        name: username.value
+        name: usernameInput.value
     };
 
+    // Pushing the score object to the highScores array
     highScores.push(score);
 
-    /* Sorting the high scores in descending order. */
-    highScores.sort((a, b) => {
+    /* Sorting the high scores in descending order based on the score value. */
+    highScores.sort(function(a, b) {
         return b.score - a.score;
     });
 
     /* Removing the last element of the array if it exceeds the maximum allowed high scores. */
-    highScores.splice(MAX_HIGH_SCORES);
+    highScores.splice(maxHighScores);
 
+    // Storing the updated highScores array in local storage.
     localStorage.setItem('highScores', JSON.stringify(highScores));
+
+    // Redirecting user back to the index.html page
     window.location.assign('index.html');
-};
+}
+
+// Adding a click event listener to the saveScoreBtn
+saveScoreBtn.addEventListener('click', saveHighScore);
