69e3416003c1ecab7ebe771d1013d0f8666c9bd1
Charltonortega <charltonortega14@gmail.com>
2023-07-12 22:32:07 +0930

    feat:add descriptive comments for better code readability

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -1,15 +1,27 @@
 // Selecting DOM elements
-
-var highScoresList = document.querySelector('#highScoresList')
-var highScores = JSON.parse(localStorage.getItem('highScores')) || []
+var highScoresList = document.querySelector('#highScoresList');
 var clearScoresButton = document.querySelector('#clear-scores-button');
 
+// adds eventlistener, removees high scores from local storage then reloads the page to update.
 clearScoresButton.addEventListener('click', function() {
     localStorage.removeItem('highScores');
     location.reload();
 });
-// Generating HTML for each high score and populating the highScoresList element
-highScoresList.innerHTML = highScores.map(score => {
-    return `<li class="high-score">${score.name} - ${score.score}</li>`
-}).join('')
 
+// Populate the highScoresList element with high scores
+function populateHighScores() {
+    // retrieves the highcore from local storage and generates a list for each user score./ 
+    var highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+    var highScoreHTML = '';
+    //create loop to populate the list
+    for (var i = 0; i < highScores.length; i++) {
+        var score = highScores[i];
+
+        //adds the score to the list
+        highScoreHTML += `<li class="high-score">${score.name} - ${score.score}</li>`;
+    }
+    //assigns the values to the list
+    highScoresList.innerHTML = highScoreHTML;
+}
+// calls function to populate high scores in quiz
+populateHighScores();
